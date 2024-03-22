5b10ee804b41b932c07d49a04fba899332ab83ba
mdfaiz903 <mdfaiz.nub@gmail.com>
2024-03-23 00:31:20 +0600

    model migration done and res api configration done

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    text = models.CharField(max_length=150)
+    date = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.text
